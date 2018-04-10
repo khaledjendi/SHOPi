8d0179de8cc43d83ab1ab3b08d240c50ef8ebc03
Khaled Jendi <khaledj@kth.se>
2018-04-10 13:23:50 +0200

    adding carosel

--- src/app/home/man-home-category/man-home-category.component.ts
+++ src/app/home/man-home-category/man-home-category.component.ts
@@ -1,6 +1,10 @@
 import { expandCollapse } from '../home-category.animations';
 import { Component, OnInit, Injectable, Output, EventEmitter } from '@angular/core';
 
+import { Input, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+
+declare var jQuery: any;
+
 @Component({
   selector: 'app-man-home-category',
   templateUrl: './man-home-category.component.html',
@@ -8,7 +12,7 @@ import { Component, OnInit, Injectable, Output, EventEmitter } from '@angular/co
   animations: [expandCollapse]
 })
 
-export class ManHomeCategoryComponent implements OnInit {
+export class ManHomeCategoryComponent implements OnInit, AfterViewInit   {
   private menuParts = {
     newArrival: {
       isInHeader: false,
@@ -33,6 +37,83 @@ export class ManHomeCategoryComponent implements OnInit {
   }
   hideOverlay: boolean = true;
   type: string = "";
+
+
+  @ViewChild('slider') slider: ElementRef;
+
+  ngAfterViewInit() {
+    jQuery(this.slider.nativeElement).revolution({
+      sliderType: "standard",
+      jsFileLocation: "revolution/js/",
+      sliderLayout: "auto",
+      dottedOverlay: "none",
+      delay: 9000,
+      navigation: {
+        keyboardNavigation: "off",
+        keyboard_direction: "horizontal",
+        mouseScrollNavigation: "off",
+        mouseScrollReverse: "default",
+        onHoverStop: "on",
+        touch: {
+          touchenabled: "on",
+          swipe_threshold: 75,
+          swipe_min_touches: 50,
+          swipe_direction: "horizontal",
+          drag_block_vertical: false
+        }
+        ,
+        arrows: {
+          style: "gyges",
+          enable: true,
+          hide_onmobile: false,
+          hide_onleave: false,
+          tmp: '',
+          left: {
+            h_align: "right",
+            v_align: "bottom",
+            h_offset: 40,
+            v_offset: 0
+          },
+          right: {
+            h_align: "right",
+            v_align: "bottom",
+            h_offset: 0,
+            v_offset: 0
+          }
+        }
+      },
+      responsiveLevels: [1240, 1024, 778, 480],
+      visibilityLevels: [1240, 1024, 778, 480],
+      gridwidth: [1200, 1024, 778, 480],
+      gridheight: [600, 600, 600, 600],
+      lazyType: "single",
+      parallax: {
+        type: "scroll",
+        origo: "slidercenter",
+        speed: 400,
+        levels: [5, 10, 15, 20, 25, 30, 35, 40, 45, 46, 47, 48, 49, 50, 51, 55]
+      },
+      shadow: 0,
+      spinner: "off",
+      stopLoop: "off",
+      stopAfterLoops: -1,
+      stopAtSlide: -1,
+      shuffle: "off",
+      autoHeight: "off",
+      disableProgressBar: "on",
+      hideThumbsOnMobile: "off",
+      hideSliderAtLimit: 0,
+      hideCaptionAtLimit: 0,
+      hideAllCaptionAtLilmit: 0,
+      debugMode: false,
+      fallbacks: {
+        simplifyAll: "off",
+        nextSlideOnWindowFocus: "off",
+        disableFocusListener: false,
+      }
+    });
+  }
+
 
   constructor() {
 

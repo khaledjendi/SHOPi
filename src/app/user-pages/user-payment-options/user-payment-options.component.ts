c158748cbeeaa34a4d43172aaa3107c97f2c94da
Khaled Jendi <khaledj@kth.se>
2018-05-07 14:11:07 +0200

    user payment options page is done with rearrange (sort) cards by draging and dropping them

--- src/app/user-pages/user-payment-options/user-payment-options.component.ts
+++ src/app/user-pages/user-payment-options/user-payment-options.component.ts
@@ -6,6 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-payment-options.component.scss']
 })
 export class UserPaymentOptionsComponent implements OnInit {
+  cards = [
+    {
+      no: '**** **** **** 5021',
+      background: 'rgba(144, 238, 144, 0.6)'
+    }, {
+      no: '**** **** **** 3891',
+      background: 'rgba(255, 192, 203, 0.6)'
+    }, {
+      no: '**** **** **** 9461',
+      background: 'rgba(244, 164, 96, 0.6)'
+    }]
 
   constructor() { }
 

0ea57ace78c4b9a347fcd51ff6a36e89d95214b2
Khaled Jendi <khaledj@kth.se>
2018-05-07 21:42:02 +0200

    add new card to user's payment cards

--- src/app/user-pages/user-payment-options/user-payment-options.component.ts
+++ src/app/user-pages/user-payment-options/user-payment-options.component.ts
@@ -1,3 +1,5 @@
+import { LoginErrorStateMatcher } from './../../login-auth/login/login.component';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -18,9 +20,67 @@ export class UserPaymentOptionsComponent implements OnInit {
       background: 'rgba(244, 164, 96, 0.6)'
     }]
 
+  addCardForm = new FormGroup({
+    name: new FormControl('', [
+      Validators.required
+    ]),
+    number: new FormControl('', [
+      Validators.required,
+      Validators.minLength(16)
+    ]),
+    month: new FormControl('', [
+      Validators.required
+    ]),
+    year: new FormControl('', [
+      Validators.required
+    ])
+  });
+
+  matcher = new LoginErrorStateMatcher();
+
+  get name() {
+    return this.addCardForm.get('name')
+  }
+
+  get number() {
+    return this.addCardForm.get('number')
+  }
+
+  get month() {
+    return this.addCardForm.get('month')
+  }
+
+  get year() {
+    return this.addCardForm.get('year')
+  }
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+
+    if (this.addCardForm.valid) {
+      let number = this.addCardForm.get('number').value;
+
+      this.cards.push({
+        no: this.formatCard(number),
+        background: 'rgba(' + (Math.floor(Math.random() * 256) + 70) + ',' + (Math.floor(Math.random() * 256) + 70) + ',' + (Math.floor(Math.random() * 256) + 70) + ',0.6)'
+      })
+    }
+  }
+
+  formatCard(number) {
+    if (number.length >= 16) {
+      let formattedNumber = "";
+      for (let i = 0; i < number.length; i++) {
+        formattedNumber = (i !== 0 && (i % 4) === 0) ? formattedNumber + " " : formattedNumber;
+        formattedNumber = i < 12 ? formattedNumber + "*" : formattedNumber + number[i];
+      }
+      return formattedNumber;
+    }
+    return number;
   }
 
 }
